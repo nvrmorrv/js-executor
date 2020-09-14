5be9d15056aa1bfdcfca2114e30dd3f44b8b8e96
Denys <den98lev@gmail.com>
2020-09-14 23:39:54 +0300

    executors bug fix

--- src/main/java/impl/App.java
+++ src/main/java/impl/App.java
@@ -5,8 +5,10 @@ package impl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class App {
   public static void main(String[] args) {
    SpringApplication.run(App.class, args);
--- src/main/java/impl/configs/AppConfig.java
+++ src/main/java/impl/configs/AppConfig.java
@@ -1,19 +1,12 @@
 package impl.configs;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,11 +15,10 @@ import org.zalando.problem.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
-@EnableAsync
-public class AppConfig implements AsyncConfigurer {
+public class AppConfig {
 
   @Bean
-  public WebMvcConfigurer webConfig(ConcurrentTaskExecutor concurrentTaskExecutor) {
+  public WebMvcConfigurer webConfig(Executor applicationTaskExecutor) {
     return new WebMvcConfigurer() {
       @Override
       public void addInterceptors(InterceptorRegistry registry) {
@@ -37,31 +29,9 @@ public class AppConfig implements AsyncConfigurer {
 
       @Override
       public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        configurer.setTaskExecutor(concurrentTaskExecutor);
+        configurer.setTaskExecutor(new ConcurrentTaskExecutor(applicationTaskExecutor));
       }
     };
-  }
-
-  @Bean
-  protected ConcurrentTaskExecutor getTaskExecutor(@Qualifier("taskExecutor") Executor taskExecutor) {
-    return new ConcurrentTaskExecutor(taskExecutor);
-  }
-
-  @Bean(name = "taskExecutor")
-  @Override
-  public Executor getAsyncExecutor() {
-    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(4);
-    executor.setMaxPoolSize(4);
-    executor.setQueueCapacity(500);
-    executor.initialize();
-    return executor;
-  }
-
-
-  @Bean(name = "AsyncExecutor")
-  public ExecutorService service(@Value("${executor.thread-count}") int threadCount) {
-    return Executors.newFixedThreadPool(threadCount);
   }
 
   @Bean
