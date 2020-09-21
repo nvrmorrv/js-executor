0272a2500fcc14f7f8595c7221a175f9bfb6d036
denys <den98lev@gmail.com>
2020-09-21 15:09:24 +0300

    removing setting async executor

--- src/main/java/impl/configs/AppConfig.java
+++ src/main/java/impl/configs/AppConfig.java
@@ -1,15 +1,11 @@
 package impl.configs;
 
-import java.util.concurrent.Executor;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
@@ -20,18 +16,13 @@ import org.zalando.problem.violations.ConstraintViolationProblemModule;
 public class AppConfig {
 
   @Bean
-  public WebMvcConfigurer webConfig(Executor applicationTaskExecutor) {
+  public WebMvcConfigurer webConfig() {
     return new WebMvcConfigurer() {
       @Override
       public void addInterceptors(InterceptorRegistry registry) {
         WebContentInterceptor interceptor = new WebContentInterceptor();
         interceptor.addCacheMapping(CacheControl.noStore().noTransform(), "/*");
         registry.addInterceptor(interceptor);
-      }
-
-      @Override
-      public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        configurer.setTaskExecutor(new ConcurrentTaskExecutor(applicationTaskExecutor));
       }
     };
   }
